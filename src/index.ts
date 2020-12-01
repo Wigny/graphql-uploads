0bac13ed8edfdfd616160587a1b998a5c8f14d3a
Wígny Almeida <wigny.almeida@credisis.com.br>
2020-12-01 18:46:27 -0300

    GraphQl Hello World

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello world");
+import { ApolloServer } from "apollo-server";
+import gql from "graphql-tag";
+
+const typeDefs = gql`
+  type Query {
+    hello:  String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const schema = new ApolloServer({ typeDefs, resolvers });
+
+schema.listen({ port: process.env.PORT || 8000 }).then(({ url }) => {
+  console.log(`schema ready at ${url}`);
+});
