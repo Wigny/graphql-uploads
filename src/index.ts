1da7bee76b5986735424e35b3b9f7488b0b1283f
Wígny Almeida <wigny.almeida@credisis.com.br>
2020-12-02 17:09:58 -0300

    Upload to Azure

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
+import { BlobServiceClient } from '@azure/storage-blob';
 import { ApolloServer, gql } from 'apollo-server';
+import { v4 as uuid } from 'uuid';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { PORT, CONNECT_STR, CONTAINER_NAME, ACCOUNT_NAME } = process.env;
 
 const typeDefs = gql`
   type File {
     filename: String!
     mimetype: String!
-    encoding: String!
+    url: String!
   }
 
   type Query {
@@ -21,19 +29,44 @@ const resolvers = {
     running: () => true,
   },
   Mutation: {
-    upload: async (parent: any, args: any) => {
-      const files = await args.files;
+    upload: async (_parent: any, args: any) => {
+      const results: any[] = [];
 
-      return files;
+      // tslint:disable-next-line:prefer-for-of
+      for await (const file of args.files) {
+        const result = await upload(file);
+
+        results.push(result)
+      }
+
+      return results;
     },
   },
 };
+
+const upload = async ({ filename, mimetype, createReadStream }: any) => {
+  const stream = createReadStream();
+  const file = uuid() + path.extname(filename);
+
+  await BlobServiceClient
+    .fromConnectionString(CONNECT_STR as string)
+    .getContainerClient(CONTAINER_NAME as string)
+    .getBlobClient(file)
+    .getBlockBlobClient()
+    .uploadStream(stream);
+
+  return {
+    filename: file,
+    mimetype,
+    url: `https://${ACCOUNT_NAME}.blob.core.windows.net/files/${file}`,
+  }
+}
 
 const server = new ApolloServer({
   resolvers,
   typeDefs,
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: PORT }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
